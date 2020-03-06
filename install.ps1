0817b33f6674153e47a161612b0646481a27e00c
Jan Dvorak <jan.dvorak@nanoenergies.cz>
2020-03-06 14:54:07 +0100

    fixed install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,3 @@
-param (
-    [string][Parameter(Mandatory=$true)] $SlnFile
-)
-
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 Copy-Item '.gitattributes' -Destination '..' -Force
 Copy-Item '.gitignore' -Destination '..' -Force
